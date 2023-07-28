140df13b2e606a4409410234a4c86d019bf14fd5
Ryan Smith <ryan.smith1@bp.com>
2023-07-28 17:21:08 +0100

    feat: Setup initial functions and image conversion

---
+++ src/shared/index.ts
@@ -0,0 +1,13 @@
+import sharp from 'sharp';
+
+// !Important sharp is not working when built for some reason
+// Example code for the AWS Lambdas to reduce uploaded image size
+// This reduced the size 6.6MB => 13KB which is a huge saving
+//convertImageToWebpFormat('large-image.jpeg', 300, 'test.webp');
+export const convertImageToWebpFormat = async (
+    fileName: string,
+    width: number,
+    outputFileName: string,
+): Promise<void> => {
+    await sharp(fileName).resize(width).webp().toFile(outputFileName);
+};
