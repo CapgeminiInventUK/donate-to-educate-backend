adef554ec4cdb1c5fb2c00628d3044da2b2db538
Ryan Smith <ryan.smith1@bp.com>
2023-10-20 10:46:08 +0100

    feat: got csv working in lambda

--- src/shared/file.ts
+++ src/shared/file.ts
@@ -1,7 +1,8 @@
-import { readdirSync, rmSync, unlinkSync, readFileSync } from 'fs';
+import { readdirSync, rmSync, unlinkSync } from 'fs';
 import { unzip } from './zip';
 import { logger } from './logger';
-import { ParseResult, parse } from 'papaparse';
+import csv from 'csvtojson';
+import { school_data_headers } from './globals';
 
 export const loadCsvDataFromZip = async <T>(zipFile: string, extractPath: string): Promise<T> => {
   try {
@@ -9,12 +10,13 @@ export const loadCsvDataFromZip = async <T>(zipFile: string, extractPath: string
 
     const filename = readdirSync(extractPath).pop();
     const filepath = `${extractPath}/${filename}`;
-    const file = readFileSync(filepath, { encoding: 'utf8' });
 
-    const { data, errors } = await parseFile<ParseResult<string[]>>(file);
-    if (errors.length > 0) {
-      throw new Error(errors.join());
-    }
+    const data = await csv({
+      trim: true,
+      noheader: false,
+      headers: school_data_headers,
+    }).fromFile(filepath);
+
     return data as T;
   } catch (error) {
     logger.error(error, 'There was a problem extracting from the zip file');
@@ -23,16 +25,4 @@ export const loadCsvDataFromZip = async <T>(zipFile: string, extractPath: string
     unlinkSync(zipFile);
     rmSync(extractPath, { recursive: true, force: true });
   }
-};
-
-const parseFile = <T>(rawFile: string): Promise<T> => {
-  return new Promise<T>((resolve) => {
-    parse(rawFile, {
-      delimiter: ',',
-      complete: (results) => {
-        resolve(results as T);
-      },
-      header: true,
-    });
-  });
 };
