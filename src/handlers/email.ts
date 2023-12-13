815dcfdd5a35016f26836f805e26c97abe516393
Ryan Smith <ryan.smith1@bp.com>
2023-12-13 21:18:18 +0000

    feat: add simply code to send an email

--- src/handlers/email.ts
+++ src/handlers/email.ts
@@ -1,7 +1,33 @@
 import { Handler } from 'aws-lambda';
+import SESClient from 'aws-sdk/clients/sesv2';
 
-export const handler: Handler = (event, context, callback): void => {
+const sesClient = new SESClient({ region: 'eu-west-2' });
+
+export const handler: Handler = async (event, context, callback): Promise<void> => {
   // eslint-disable-next-line no-console
   console.log(event);
+  try {
+    const res = await sesClient
+      .sendEmail({
+        FromEmailAddress: 'ryan.b.smith@capgemini.com',
+        Destination: { ToAddresses: ['ryan.b.smith@capgemini.com'] },
+        Content: {
+          Template: {
+            TemplateName: 'Test',
+            TemplateData: JSON.stringify({
+              name: 'Ryan',
+              favoriteanimal: 'Tiger',
+            }),
+          },
+        },
+      })
+      .promise();
+    // eslint-disable-next-line no-console
+    console.log(res);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+
   callback(null, 'Finished');
 };
