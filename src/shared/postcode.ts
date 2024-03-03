29ee0aadbd657ab4e738efe13c79cb2d97e81cd1
Ryan Smith <ryan.smith1@bp.com>
2024-03-03 13:39:36 +0000

    feat: add full url path

--- src/shared/postcode.ts
+++ src/shared/postcode.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const convertPostcodeToLatLng = async (postcode: string): Promise<[number, number]> => {
   const { data } = await axios.get<{ result: { longitude: number; latitude: number } }>(
-    `api.postcodes.io/postcodes/${postcode}`
+    `https://api.postcodes.io/postcodes/${postcode}`
   );
   const { longitude, latitude } = data.result;
   return [longitude, latitude];
