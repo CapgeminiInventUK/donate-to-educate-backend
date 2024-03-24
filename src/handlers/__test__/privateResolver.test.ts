24f3000e954597e90b8fd6e297cb3b97d4fddfdd
Ryan Smith <ryan.smith1@bp.com>
2024-03-24 21:59:17 +0000

    feat: add la user tests

--- src/handlers/__test__/privateResolver.test.ts
+++ src/handlers/__test__/privateResolver.test.ts
@@ -1,7 +1,11 @@
 import { handler } from '../privateResolver';
-import { generateContext, generateEvent } from '../../shared/testUtils';
+import { dropDatabase, generateContext, generateEvent } from '../../shared/testUtils';
 
 describe('Private Resolver', () => {
+  afterEach(async () => {
+    await dropDatabase();
+  });
+
   it('Throws error if field name does not match', async () => {
     await expect(async () => {
       await handler(generateEvent('missingFieldName'), generateContext(), jest.fn());
--- src/handlers/__test__/publicResolver.test.ts
+++ src/handlers/__test__/publicResolver.test.ts
@@ -1,8 +1,17 @@
-import { QueryGetSchoolByNameArgs, School } from '../../../appsync';
+import {
+  LocalAuthorityUser,
+  QueryGetLocalAuthorityUserArgs,
+  QueryGetSchoolByNameArgs,
+  School,
+} from '../../../appsync';
 import { handler } from '../publicResolver';
-import { generateContext, generateEvent, insertData } from '../../shared/testUtils';
+import { dropDatabase, generateContext, generateEvent, insertData } from '../../shared/testUtils';
 
 describe('Public Resolver', () => {
+  afterEach(async () => {
+    await dropDatabase();
+  });
+
   it('Throws error if field name does not match', async () => {
     await expect(async () => {
       await handler(generateEvent('missingFieldName'), generateContext(), jest.fn());
@@ -42,5 +51,49 @@ describe('Public Resolver', () => {
     }).rejects.toThrow('An unknown error occurred');
 
     expect(callback).toHaveBeenCalledWith(null, { name: 'Farlingaye', urn: '0' });
+  });
+
+  it('Get La User By Email - No results', async () => {
+    const callback = jest.fn();
+
+    await expect(async () => {
+      await handler(
+        generateEvent<QueryGetLocalAuthorityUserArgs>('getLocalAuthorityUser', ['email'], {
+          email: 'mock@example.com',
+        }),
+        generateContext(),
+        callback
+      );
+    }).rejects.toThrow('An unknown error occurred');
+
+    expect(callback).toHaveBeenCalledWith(null);
+  });
+
+  it('Get La User By Email - Results', async () => {
+    const callback = jest.fn();
+    await insertData<LocalAuthorityUser>('LocalAuthorityUser', [
+      {
+        name: 'Hackney',
+        email: 'mock@example.com',
+        department: '',
+        firstName: '',
+        lastName: '',
+        jobTitle: '',
+        nameId: '',
+        phone: '',
+      },
+    ]);
+
+    await expect(async () => {
+      await handler(
+        generateEvent<QueryGetLocalAuthorityUserArgs>('getLocalAuthorityUser', ['name', 'email'], {
+          email: 'mock@example.com',
+        }),
+        generateContext(),
+        callback
+      );
+    }).rejects.toThrow('An unknown error occurred');
+
+    expect(callback).toHaveBeenCalledWith(null, { name: 'Hackney', email: 'mock@example.com' });
   });
 });
