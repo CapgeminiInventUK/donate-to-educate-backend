71c7f58fc1f6b3d390d127363e79277c8398234d
Jake Readman <jakereadman@gmail.com>
2024-05-15 19:32:54 +0100

    la not registered

--- src/handlers/publicResolver.ts
+++ src/handlers/publicResolver.ts
@@ -151,8 +151,8 @@ export const handler: AppSyncResolverHandler<
         const hasJoinRequest = schoolJoinRequests.some(
           ({ school: schoolName }) => schoolName === name
         );
-        const registrationState = isLocalAuthorityRegistered
-          ? 'laRegistered'
+        const registrationState = !isLocalAuthorityRegistered
+          ? 'laNotRegistered'
           : hasJoinRequest
             ? 'hasJoinRequest'
             : undefined;
