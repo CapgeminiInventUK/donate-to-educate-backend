7b537b415b14996d98888c6c46d6fd0f749ca7bf
Ryan Smith <ryan.smith1@bp.com>
2024-08-05 16:02:41 +0100

    feat: only run on local

--- src/shared/middleware/responseSize.ts
+++ src/shared/middleware/responseSize.ts
@@ -1,8 +1,8 @@
-import middy from '@middy/core';
+import { MiddlewareObj } from '@middy/core';
 import sizeof from 'object-sizeof';
 import { logger } from '../logger';
 
-export const responseSize = (): middy.MiddlewareObj => {
+export const responseSize = (): MiddlewareObj => {
   return {
     after: (request) => {
       logger.info(`Size of response ${sizeof(request)} bytes`);
