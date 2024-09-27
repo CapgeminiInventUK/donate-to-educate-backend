7b291866c67164183e082acb8befe3cf5f249c25
Jake Readman <jakereadman@gmail.com>
2024-09-27 13:26:01 +0100

    don't send new la user email if additional user

--- src/handlers/email/newLaUser.ts
+++ src/handlers/email/newLaUser.ts
@@ -1,15 +1,24 @@
 import { generate } from 'randomstring';
 import { LocalAuthorityUser } from '../../../appsync';
+import { LocalAuthorityUserRepository } from '../../repository/localAuthorityUserRepository';
 import { SignUpDataRepository } from '../../repository/signUpDataRepository';
 import { checkIfDefinedElseDefault } from '../../shared/check';
 import { sendEmail } from '../email';
 
 const signUpDataRepository = SignUpDataRepository.getInstance();
+const localAuthorityUserRepository = LocalAuthorityUserRepository.getInstance();
 
 export const handleNewLaUser = async (fullDocument: LocalAuthorityUser) => {
+  const { email, firstName, name, nameId } = fullDocument;
+
+  const laUsers = await localAuthorityUserRepository.getAllById(nameId);
+
+  if (laUsers && laUsers?.length > 0) {
+    return;
+  }
+
   const domainName = checkIfDefinedElseDefault(process?.env?.DOMAIN_NAME);
   const randomString = generate({ charset: 'alphabetic', length: 100 });
-  const { email, firstName, name, nameId } = fullDocument;
 
   await signUpDataRepository.insert({
     id: randomString,
